5d11ef51f217973452b0d9589e878b4ea39a63f9
Konrad Holowaty <147845316+moneyforfun@users.noreply.github.com>
2025-06-24 23:25:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-from snowflake.snowpark.context import get_active_session
+
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -10,6 +10,8 @@ st.write(
   """
 )
 
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 myname = st.text_input('Please provide your name below:')
 st.write(f'"{myname}" will be displayed on your order')

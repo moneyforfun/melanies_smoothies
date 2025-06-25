0894f4e41dd64b381c4c6d5a84d35b2fdfd78c19
Konrad Holowaty <147845316+moneyforfun@users.noreply.github.com>
2025-06-25 22:31:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,10 @@ session = Session.builder.configs(connection_parameters).create()
 myname = st.text_input('Please provide your name below:')
 st.write(f'"{myname}" will be displayed on your order')
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')
+                                                                      
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients: ',
     my_dataframe,max_selections= 5
